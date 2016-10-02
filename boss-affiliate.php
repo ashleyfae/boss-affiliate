431f0fbec8c66ae3b6a4bdfc148e84c0aedd022f
Ashley Evans <cazzyae@gmail.com>
2016-10-02 10:15:36 +0100

    Added main plugin file and license.

---
+++ boss-affiliate.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: Boss Affiliate
+ * Plugin URI: https://github.com/nosegraze/boss-affiliate
+ * Description: Automatically add your affiliate disclosure to your blog posts.
+ * Version: 1.0
+ * Author: Boss Plugins
+ * Author URI: https://bossplugins.com
+ * License: GPL2
+ *
+ * @package   boss-affiliate
+ * @copyright Copyright (c) 2016, Ashley Gibson
+ * @license   GPL2+
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
